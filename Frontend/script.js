764c60528cdd846d9f0348402cd1c1d6b43ef95a
CamiloGuengue <guenguec@gmail.com>
2026-01-29 21:35:41 -0500

    fix json-server fetch

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,3 +1,5 @@
+import {fetchGet} from './modules/fetch.js'
+
 document.addEventListener('DOMContentLoaded', () => {
   const tabs = document.querySelectorAll('.tab');
   const views = document.querySelectorAll('.auth-view');
@@ -20,8 +22,27 @@ btnLogin.addEventListener('click',() =>{
     const takePassword = document.getElementById('password').value
     console.log(takeEmail);
     console.log(takePassword);
-    login(takeEmail,takePassword)
+    login(takeEmail);
 
 })
 
+async function login(email) {
+
+    fetchGet('users')
+    .then(result => {
+        console.log(result[0].email)
+        console.log(result);
+        result.forEach(element => {
+            if(element.email === email){
+                alert("correo finded")
+            }
+            console.log(element.id);
+            console.log(element.name);
+            console.log(element.rol);
+            
+        });
+    })
+    .catch(error => console.log(error));
+    
+}
 
